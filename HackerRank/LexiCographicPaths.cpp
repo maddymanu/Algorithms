e934a3338f533ec4701846ad43deb46ee96c1d28
Aditya Bansal <maddymanu@hotmail.com>
2014-08-29 19:05:41 +0530

    YAY! Lexocographic work.

--- HackerRank/LexiCographicPaths.cpp
+++ HackerRank/LexiCographicPaths.cpp
@@ -1,25 +1,32 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <vector>
+using namespace std;
 
 int n, m;
+
+vector<string> strVector;
 
 int findWays(int x, int y, std::string str)
 {
     if (x <= n && y <= m)
-    {
-        str += "(";
-        str += std::to_string(x);
-        str += ", ";
-        str += std::to_string(y);
-        str +="), ";
+    {   
+        if(!(x==0 && y==0)) {
+            //str += "(";
+            //str += std::to_string(x);
+            //str += ", ";
+            //str += std::to_string(y);
+            //str +="), ";
+        }
         if (x == n && y == m)
         {
-            std::cout << "Path End : " << str << std::endl;
+            //std::cout << "Path End : " << str << std::endl;
+            strVector.push_back(str);
             return 1;
         }
-        int right = findWays(x + 1, y, str);
-        int down = findWays(x, y + 1, str);
+        int right = findWays(x + 1, y, str+"H");
+        int down = findWays(x, y + 1, str+"V");
         return (right + down);
     }
     else
@@ -30,10 +37,22 @@ int findWays(int x, int y, std::string str)
 
 int main()
 {
-    n = 2;
-    m = 2;
-    std::cout << "Paths :\n";
-    int count = findWays(0,0,"");
-    std::cout << " Total no of Paths : " << count << std::endl;
+    // n = 2;
+    // m = 2;
+    // int count = findWays(0,0,"");
+    // sort(strVector.begin() , strVector.end());
+    // cout << strVector.at(2) << endl;
+    // 
+    long long int T;
+    int K;
+    cin >> T;
+    while(T--) {
+        strVector.clear();
+        cin >> n >> m >> K;
+        int count = findWays(0,0,"");
+        sort(strVector.begin() , strVector.end());
+        cout << strVector.at(K) << endl;
+    }
+
     return 0;
 }
