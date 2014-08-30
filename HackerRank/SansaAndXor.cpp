bee5d82a05623f0dbd6db90cd37d4ab17598de32
Aditya Bansal <maddymanu@hotmail.com>
2014-08-30 18:50:23 +0530

    SansaAndXor Doesn't Work.!

--- HackerRank/SansaAndXor.cpp
+++ HackerRank/SansaAndXor.cpp
@@ -1,15 +1,8 @@
 #include <iostream>
-#include <stdio.h>
 #include <assert.h> 
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <algorithm>
 #include <string>
 using namespace std;
-
-
-vector<int> L; 
 
 long long int ans = 0;
 
@@ -37,18 +30,15 @@ void findAllSubstrings(const char *s){
 int main() {
 
 	string str = "";
-	// cin >> str;
-	// const char *pt = str.c_str();
-	// findAllSubstrings(pt);
-	// cout << "ans is= " << ans << endl;
 	int T;
 	cin >> T;
-	int N;
+	assert(T>=1 && T<=5);
+	long long int N;
 	while(T--) {
 		ans = 0;
 		str = "";
 		cin >> N;
-		//int arr[N];
+		assert(N>=2 && N<=100000);
 		for(int i=0 ; i<N ; i++) {
 			char c ;
 			cin >> c;
