c2ef8fb77aed5b8be7475601aff4d5d1eeb13ed9
Aditya Bansal <maddymanu@hotmail.com>
2014-08-30 10:03:27 +0530

    SansaAndXor Submitted. Low score.

--- HackerRank/SansaAndXor.cpp
+++ HackerRank/SansaAndXor.cpp
@@ -11,12 +11,21 @@ using namespace std;
 
 vector<int> L; 
 
+long long int ans = 0;
+
+void xorAllSubstrings(char c) {
+	// /cout << "ANS=Char= " << ans << " " << (c-'0') ;
+	ans = ans ^ (c-'0');
+}
+
 void findAllSubstrings(const char *s){
     int x=0;
     while(*(s+x)){
-        for(int y=0; y<=x; y++)
-            cout<<*(s+y);
-        cout<<'\n';
+        for(int y=0; y<=x; y++) {
+        	//cout<<*(s+y);
+        	xorAllSubstrings(*(s+y));
+        }
+        //cout<<'\n';
         x++;
     }
     if(*(s+1))
@@ -27,9 +36,51 @@ void findAllSubstrings(const char *s){
 
 int main() {
 
-	string str;
-	cin >> str;
-	const char *pt = str.c_str();
-	findAllSubstrings(pt);
+	string str = "";
+	// cin >> str;
+	// const char *pt = str.c_str();
+	// findAllSubstrings(pt);
+	// cout << "ans is= " << ans << endl;
+	int T;
+	cin >> T;
+	int N;
+	while(T--) {
+		ans = 0;
+		str = "";
+		cin >> N;
+		//int arr[N];
+		for(int i=0 ; i<N ; i++) {
+			char c ;
+			cin >> c;
+			str = str+c;
+		}
+		//cout << str;
+		const char *pt = str.c_str();
+	    findAllSubstrings(pt);
+	    cout << ans << endl;
+	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
