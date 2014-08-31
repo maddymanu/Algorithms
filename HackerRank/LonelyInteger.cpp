d6d85858f857fc4221fb0c0ce4b994b8ad334a5e
Aditya Bansal <maddymanu@hotmail.com>
2014-08-31 11:12:35 +0530

    LonelyIntegers Complete.

---
+++ HackerRank/LonelyInteger.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	int arr[n];
+	for(int i=0 ; i<n ; i++) {
+		cin >> arr[i];
+	}
+	int ans = 0;
+	for(int i=0 ; i<n ; i++) {
+		ans = ans ^ arr[i];
+	}
+	cout << ans << endl;
+	return 0;
+}
