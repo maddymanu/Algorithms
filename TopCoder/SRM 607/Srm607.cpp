7d55d3d1d737b0857ee29d8de65e093af962974b
Aditya Bansal <maddymanu@hotmail.com>
2014-09-05 09:56:06 +0530

    Started SRM 607.

---
+++ TopCoder/SRM 607/Srm607.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+// Div2 250
+class BoundingBox
+{
+public:
+	int smallestArea(vector<int> X, vector<int> Y) {
+		int minx = *min_element(X.begin(), X.end());
+		int maxx = *max_element(X.begin(), X.end());
+    	int miny = *min_element(Y.begin(), Y.end());
+    	int maxy = *max_element(Y.begin(), Y.end());
+    	return (maxx-minx) * (maxy - miny) ;
+	}
+	
+};
