f379e874675da25676dee8fae6f7fdf15271d4b7
Aditya Bansal <maddymanu@hotmail.com>
2014-09-11 10:02:46 +0530

    Completed Simulation.Do more.

---
+++ TopCoder/Categories/SimulationSetOne.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+//Div2 250
+class GymTraining
+{
+public:
+	int trainingTime(int needToTrain, int minPulse, int maxPulse, int trainChange, int restChange) {
+		int time = 0;
+		int curr = minPulse;
+		if(minPulse+trainChange > maxPulse) {
+			return -1;
+		}
+
+		while(needToTrain > 0 ) {
+			needToTrain--;
+			if(curr + trainChange <= maxPulse) {
+				curr = curr+trainChange;
+				need--;
+			} else {
+				curr = min(minPulse, curr-rest);
+			}
+		}
+
+		return time;
+
+	}
+	
+};
+
+//Div2 250
+class CountGame
+{
+public:
+	int howMany(int maxAdd, int goal, int next) {
+		for(int i=goal ; i>=next ; i-=(maxAdd+1)) {
+			if(i-next+1 <= maxAdd) {
+				return i-next+1;
+			}
+		}
+		return -1;
+	}
+	
+};
