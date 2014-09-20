1af5a8b13563c886b6f5309fff6c3b93dea7ddad
Aditya Bansal <maddymanu@hotmail.com>
2014-09-20 15:56:12 +0530

    Did the first 1000ptr problem.

--- TopCoder/Categories/DynamicProgrammingSetOne.cpp
+++ TopCoder/Categories/DynamicProgrammingSetOne.cpp
@@ -69,7 +69,37 @@ public:
 };
 
 
+//Div2 1000pts -- First 1000pt problem.
 
+class UnsealTheSafe
+{
+public:
+	long long countPasswords(int N) {
+		long long d[50][20];
+		int i=0;
+		for(i = 0; i<=9 ; i++) {
+			d[1][i] = 1;
+		}
+		for(i=2 ; i<=N ; i++) {
+			d[i][0] = d[i-1][7];
+			d[i][1] = d[i-1][2] + d[i-1][4];
+			d[i][2] = d[i-1][1] + d[i-1][3] + d[i-1][5];
+			d[i][3] = d[i-1][2] + d[i-1][6];
+			d[i][4] = d[i-1][1] + d[i-1][5] + d[i-1][7];
+			d[i][5] = d[i-1][2] + d[i-1][4] + d[i-1][6] + d[i-1][8];
+			d[i][6] = d[i-1][3] + d[i-1][5] + d[i-1][9];
+			d[i][7] = d[i-1][4] + d[i-1][8] + d[i-1][0];
+			d[i][8] = d[i-1][5] + d[i-1][7] + d[i-1][9];
+			d[i][9] = d[i-1][6] + d[i-1][8];
+		}
+		long long ans=0; 
+	    for(i=0; i<=9; i++){ 
+	      ans+=d[N][i]; 
+	    } 
+	    return ans;
+	}
+	
+};
 
 
 
