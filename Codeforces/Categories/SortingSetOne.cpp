e0a9df0254ae2166d3176f552a1cda86679ec89d
Aditya Bansal <maddymanu@hotmail.com>
2014-09-27 16:59:50 +0530

    Did more sorting.

--- Codeforces/Categories/SortingSetOne.cpp
+++ Codeforces/Categories/SortingSetOne.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <algorithm>
 #include <utility>
+#include <map>
 #include <stack>
 #include <string.h>
 
@@ -46,7 +47,35 @@ using namespace std;
 
 
 
+//AmusingJoke -- 141A
 
+int main(int argc, char const *argv[])
+{
+	int arr1[26] , arr2[26];
+	memset(arr1 , 0 , sizeof(arr1));
+	memset(arr2 , 0 , sizeof(arr2));
+	string s1,s2,s3;
+	cin >> s1 >> s2 >> s3;
+	for(int i=0 ; i<s1.size() ; i++) {
+		arr1[s1[i]-'A']++;
+	}
+	for(int i=0 ; i<s2.size() ; i++) {
+		arr1[s2[i] - 'A']++;
+	}
+	for(int i=0 ; i<s3.size() ; i++) {
+		arr2[s3[i] - 'A']++;
+	}
+
+	for(int i=0 ; i<26 ; i++) {
+		if(arr1[i] != arr2[i]) {
+			cout << "NO" << endl;
+			return 0;
+		}
+	}
+
+	cout << "YES" << endl;
+	return 0;
+}
 
 
 
