240dea508905a4c5165ca9e0a2cfea9a2d75b6f7
Aditya Bansal <maddymanu@hotmail.com>
2014-12-28 10:03:41 +0530

    Started DFS!

---
+++ Codeforces/Categories/DFSSetOne.cpp
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <iostream>
+#include <math.h>
+#include <assert.h>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <map>
+#include <stack>
+#include <string.h>
+#include <cctype>
+
+using namespace std;
+
+//489B BerSU Ball
+// int main(int argc, char const *argv[])
+// {
+// 	//const int MAXN = 100 + 10;
+//     int m, n;
+
+//     int res = 0;
+
+//     cin >> n;
+//     int boys[n];
+//     for(int i=0 ; i<n ; i++) {
+//     	cin >> boys[i];
+//     }
+
+//     cin >> m;
+//     int girls[m];
+//     for(int i=0 ; i<m ; i++) {
+//     	cin >> girls[i];
+//     }
+
+//     sort(boys , boys+n);
+//     sort(girls , girls+m);
+
+//     for(int i=0 ; i<n ; i++) {
+//     	for(int j=0 ; j<m ; j++) {
+//     		if(abs(boys[i] - girls[j]) <= 1) {
+//     			res++;
+//     			girls[j] = 1000;
+//     			break;
+//     		}
+//     	}
+//     }
+
+//     cout << res << endl;
+// 	return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
