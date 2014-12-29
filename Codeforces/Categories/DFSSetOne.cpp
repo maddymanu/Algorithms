041e2323cefc4441f490a2d41e1e40b72706523c
Aditya Bansal <maddymanu@hotmail.com>
2014-12-29 12:03:52 +0530

    Did more Dp and Graphs

--- Codeforces/Categories/DFSSetOne.cpp
+++ Codeforces/Categories/DFSSetOne.cpp
@@ -9,6 +9,7 @@
 #include <stack>
 #include <string.h>
 #include <cctype>
+#include <queue>
 
 using namespace std;
 
@@ -50,7 +51,84 @@ using namespace std;
 // }
 
 
+//377A Maze
 
+
+struct node
+{
+    node(int _x,int _y){x=_x;y=_y;}
+    int x;
+    int y;
+};
+
+int main(int argc, char const *argv[])
+{
+	const int N=550;
+	char maps[N][N];
+	bool changed[N][N];
+	bool visited[N][N];
+	int dir[4][2]={{1,0},{-1,0},{0,-1},{0,1}};
+	int n,m,k;
+
+	while(cin >> n >> m >> k) {
+		memset(changed,0,sizeof(changed));
+        memset(visited,0,sizeof(visited));
+
+        int curx= 0;
+        int cury = 0;
+        int cnt = 0;
+
+        for(int i=0 ; i<n ; i++) {
+        	for(int j=0 ; j<m ; j++) {
+        		cin >> maps[i][j];
+        		if(maps[i][j] == '.') {
+        			curx = i;
+        			cury = j;
+        			cnt++;
+        		}
+        	}
+        }
+
+        int target = cnt - k;
+        queue<node> q;
+        q.push(node(curx,cury));
+        while(target > 0) {
+        	node d = q.front();
+        	q.pop();
+        	changed[d.x][d.y] = true;
+        	visited[d.x][d.y] = true;
+        	target -- ;
+        	if(target <= 0) {
+        		break;
+        	}
+        	for(int i=0 ; i<4 ; i++) {
+        		int nextx = d.x+dir[i][0];
+                int nexty = d.y+dir[i][1];
+                if(nextx>=0&&nextx<n&&nexty>=0&&nexty<m&&!visited[nextx][nexty]&&maps[nextx][nexty]=='.')
+                {
+                    q.push(node(nextx,nexty));
+                    visited[nextx][nexty]=true;
+                }
+        	}
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(maps[i][j]!='.')
+                    cout<<maps[i][j];
+                else if(changed[i][j]==false)
+                    cout<<'X';
+                else
+                    cout<<maps[i][j];
+            }
+            cout<<endl;
+        }
+	}
+	
+	return 0;
+}
 
 
 
--- Codeforces/Categories/DynamicProgrammingSetOne.cpp
+++ Codeforces/Categories/DynamicProgrammingSetOne.cpp
@@ -11,8 +11,42 @@
 
 using namespace std;
 
+//327A Flipping Game
 int main(int argc, char const *argv[])
 {
-	//Dynamic.
+	int n;
+	cin >> n;
+	vector<int> input;
+	vector<int> gain;
+	int S = 0;
+
+	for(int i=0 ; i<n ; i++) {
+		int x;
+		cin >> x;
+		input.push_back(x);
+
+		if(x == 1) {
+			gain.push_back(-1);
+			S++;
+		}else {
+			gain.push_back(1);
+		}
+	}
+
+	//maximul subarray problem on gain. + S;
+	int maxSoFar = 0 , maxHere = 0;
+	for(int i=0 ; i<n ; i++) {
+		maxHere = maxHere + gain[i];
+		if(maxHere < 0) {
+			maxHere = 0;
+		}
+		if(maxSoFar < maxHere) {
+			maxSoFar = maxHere;
+		}
+	}
+
+	int ans = maxSoFar+S;
+	cout << ans << endl;
+
 	return 0;
 }
