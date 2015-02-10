86b33c6610ace3289af5ab1738b2f82ef584384c
Aditya Bansal <maddymanu@hotmail.com>
2015-02-09 21:26:08 -0800

    Did more leetcode.

---
+++ LeetCode OJ/ClimbStairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        
+        int arr[n+1] = {};
+        if(n<=0)
+            return 0;
+        
+        arr[0] = 0;
+        arr[1] = 1;
+        arr[2] = 2;
+        
+        for(int i=3 ; i<=n ; i++) {
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+        
+        return arr[n];
+    }
+};
